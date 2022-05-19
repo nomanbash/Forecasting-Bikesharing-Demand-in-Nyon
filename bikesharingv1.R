ac6ccbdc3b70a518ad171340d11a7dedcdfaa698
nomanbash <nomanbashirmalik@gmail.com>
2022-05-19 18:41:40 +0200

    Coding Complete. Model Selected. Forecast File Outputted

--- bikesharingv1.R
+++ bikesharingv1.R
@@ -2,8 +2,8 @@
 library(fpp3)
 library(zoo)
 library(timeDate)
-library(GGally)
 library(broom)
+library(patchwork)
 
 load('Bikesharing_dataset.Rdata')
 
@@ -26,9 +26,10 @@ total$date <- as.POSIXct(total$date)
 total.ts <- as_tsibble(total, index = date, key = name, regular = TRUE)
 total.ts %>% autoplot(Bike, show.legend = FALSE) + facet_wrap(vars(name)) + ggtitle("Bikes by Station")
 total.ts <- total.ts %>% select(!Capacity)
+total.ts <- total.ts %>% mutate(TotalBikes = Bike + `E-Bike`)
+test.ts <- total.ts %>% filter(as.Date(date) >= as.Date("2022-05-11"))
 total.ts <- total.ts %>% filter(as.Date(date) < as.Date("2022-05-11"))
-
-
+test.hts <- test.ts %>% aggregate_key(name, Total = sum(TotalBikes))
 
 #we can see long periods of the same number of bikes. To deal with NAs, we'll just replace the NAs with the last value
 total.ts %>% scan_gaps()
@@ -36,10 +37,13 @@ total.ts <- total.ts %>% fill_gaps(.full = TRUE)
 total.ts <- na.locf(total.ts)
 total.ts %>% scan_gaps()
 
-#let's add a sum of the two bikes
-total.ts <- total.ts %>% mutate(TotalBikes = Bike + `E-Bike`)
-
-total.ts %>% summarize(count = sum(TotalBikes)) %>% autoplot() + ggtitle("Bike Demand in Nyon", subtitle = "Combined Bikes and E-Bikes Demand") + xlab("Time") + ylab("Count of Bikes")
+total.ts %>% 
+  summarize(count = sum(TotalBikes)) %>% 
+  autoplot() + 
+  ggtitle("Bike Demand in Nyon", 
+          subtitle = "Combined Bikes and E-Bikes Demand") + 
+  xlab("Time") + 
+  ylab("Count of Bikes")
 
 #plotting e-bikes and bikes together
 total.ts %>%
@@ -61,7 +65,11 @@ total.ts %>%
   labs(title = "Correlation between Bike Demand and E-Bike Demand", x = "Bikes", y = "E-Bikes")
 
 #Let's see if there's a correlation between e-bike count and bike-count based on location
-total.ts %>% ggplot() + geom_jitter(aes(Bike, `E-Bike`)) + facet_wrap(vars(name))
+total.ts %>% 
+  ggplot() + 
+  geom_jitter(aes(Bike, `E-Bike`)) + 
+  facet_wrap(vars(name))
+
 with(total.ts, cor(Bike, `E-Bike`))
 
 #now that we've determined that there's a fair bit off correlation, it's time to work on the dataset
@@ -89,30 +97,8 @@ ggplot(total.ts) +
   geom_boxplot(aes(x = name, y = TotalBikes, color = holiday)) +
   coord_flip()
 
-#there's clearly a difference. Some places have more demand on holidays, some have less
-# nightday <- function(datetime) {
-#   paste(
-#     c("Night", "Morning", "Morning Rush", "Morning", "Lunch", "Afternoon", "Afternoon Rush", "Evening", "Night")[
-#       cut(as.numeric(format(datetime, "%H%M")), c(0, 530, 0700, 0800, 1100, 1300, 1600, 1700 ,2000, 2359))
-#     ]
-#   )
-# }
-# 
-# 
-# total.ts$timeofday <- as.factor(nightday(total.ts$date))
-# total.ts$timeofday[total.ts$timeofday == "NA"] <- "Night"
-# total.ts$timeofday <- total.ts$timeofday %>%  droplevels()
-
-#now let's see if timeofday makes a difference:
-
-# total.ts %>%
-#   group_by(timeofday) %>% 
-#   summarize(count = mean(TotalBikes)) %>%
-#   ggplot() + geom_boxplot(aes(x = timeofday, y = count)) +
-#   labs(title = "Comparison of Time of Day", x = "Time of Day", y = "Average Bikes at Station")
-
 #loading weather data
-weather <- read.csv("Nyon.csv")
+weather <- read.csv("Data/weather_data.csv")
 weather$date_time
 
 weather <- weather %>% select(-c('tempC', 'mintempC', 'sunHour', 'moon_illumination', 'moonrise', 'moonset', 'sunrise', 'sunset',
@@ -128,8 +114,8 @@ bikes.ts <- as_tsibble(bikes, index = date, key = name, regular = TRUE)
 #we don't need bikes, e-bikes, date_time to run the model
 bikes.simplified <- bikes.ts %>% select(!c(Bike, `E-Bike`, date_time))
 
-bikes.simplified %>% ggplot() +
-  geom_jitter(aes(x = date, y = TotalBikes, color = maxtempC)) +
+bikes.simplified %>% filter(name == 'Nyon, Gare Nord') %>% ggplot() +
+  geom_line(aes(x = date, y = TotalBikes, color = maxtempC)) +
   scale_color_gradient(low = "blue", high = "red")
 
 #let's try and detect outliers in the data using PCA --APPENDIX
@@ -160,7 +146,36 @@ outlierdetection
 ggplot(outlierdetection[out_ind,]) + geom_jitter(aes(x = date, y = TotalBikes))
 #outliers are only on 28th March and 18th April. 18th April was Easter Monday hence more bikes at station"
 
+
+
+
+
+
+
+
+
 #Finally, let's test out some models
+
+#let's start with STL Decomposition
+stldcmp <- bikes.simplified %>%
+  model(
+    STL(TotalBikes,
+        robust = TRUE)
+  ) %>%
+  components()
+
+stldcmp %>% filter(name == 'Nyon, La Plage') %>% autoplot()
+
+my_dcmp_spec <- decomposition_model(
+  STL(TotalBikes,
+      robust = TRUE),
+  ETS(season_adjust ~ season("N")))
+
+fc <- bikes.simplified %>%
+  model(my_dcmp_spec)
+
+fcast.stl <- fc %>% forecast(h = "5 days")
+accuracymatrix <- fcast.stl %>% accuracy(test.ts) %>% select(1:6)
 
 #no aggregation TSLM model
 bikes.lm <- bikes.simplified %>% model(TSLM(TotalBikes ~ weekday + totalSnow_cm + uvIndex + HeatIndexC + WindChillC +
@@ -185,154 +200,186 @@ augment(bikes.lm) %>% filter(name == 'Nyon, Gare Sud') %>%
 #checking residuals --APPENDIX
 bikes.lm %>% filter(name == 'Nyon, Gare Sud') %>% gg_tsresiduals()
 
+#forecasting accuracy:
+#All right, now that we've done all of that nonsense, let's forecast!
+newdata <- read.csv("Data/newdata.csv")
+newdata <- newdata %>% select(-c('tempC', 'mintempC', 'sunHour', 'moon_illumination', 'moonrise', 'moonset', 'sunrise', 'sunset',
+                                 'DewPointC', 'pressure', 'FeelsLikeC', 'winddirDegree', 'location'))
+
+date <- seq.POSIXt(from = as.POSIXct('2022-05-11 00:00:00', tz = 'UTC'), to = as.POSIXct('2022-05-15 23:50:00', tz = 'UTC'), by = '10 min')
+date <- as.data.frame(date)
+date$joindates <- format(date$date, format = '%F %H:00:00')
+date$joindates <- as.POSIXct(date$joindates, tz = 'UTC')
+newdata$date_time <- as.POSIXct(newdata$date_time, format = '%d/%m/%Y %H:00',  tz = 'UTC')
+
+newdata <- left_join(date, newdata, by = c('joindates' = 'date_time'))
+newdata <- newdata %>% select(-joindates)
+
+#adding the relevant columns
+
+newdata$weekday <- as.factor(ifelse(isWeekday(newdata$date), "Weekday", "Weekend"))
+newdata$holiday <- 0
+
+#turning it into a tsibble and getting rid of date_time since we have date
+newdata.ts <- as_tsibble(newdata, index = date, key = name)
+
+fcast.lm <- bikes.lm %>% forecast(new_data = newdata.ts)
+fcast.lm %>% accuracy(test.ts)
+fcast.lm %>% filter(name == 'Nyon, Changins') %>% autoplot(bikes.simplified, level = 90)
+
+accuracymatrix <- rbind(accuracymatrix, fcast.lm %>% accuracy(test.ts) %>% select(1:6))
+
+
 #aggregated model
 bikes.hts <- bikes.simplified %>%
-  aggregate_key(name, Total = sum(TotalBikes), weekday = weekday, totalSnow_cm = totalSnow_cm, uvIndex = uvIndex, HeatIndexC = HeatIndexC, WindChillC = WindChillC,
-                  WindGustKmph = WindGustKmph, cloudcover = cloudcover, humidity = humidity,  precipMM = precipMM, maxtempC = maxtempC,  visibility = visibility, windspeedKmph = windspeedKmph,
-                  holiday = holiday)
-
-bikes.hts2 <- bikes.simplified %>% aggregate_key(name, Total = sum(TotalBikes))
-
-bikes.hts <- bikes.hts[!duplicated(bikes.hts[c(1,2)]),]
-bikes.hts
-
-
-#Aggregated Model --BOTTOM UP AND TOPDOWN APPROACHES
-bikes.lm.agg <- bikes.hts %>% model(tl = TSLM(Total ~ weekday + totalSnow_cm + uvIndex + HeatIndexC + WindChillC +
-                                                                         WindGustKmph + cloudcover + humidity + precipMM + maxtempC + visibility + windspeedKmph +
-                                                                         holiday))
-
-augment(bikes.TSLM2) %>% 
-  ggplot(aes(x = date)) +
-  geom_line(aes(y = Total, colour = "Data"), size = 1) +
-  geom_line(aes(y = .fitted, colour = "Fitted"), size = 1) + xlab("Year") + ylab(NULL) +
-  guides(colour = guide_legend(title=NULL))
+  aggregate_key(name, Total = sum(TotalBikes))
 
 #let's try using an ets model on the aggregated one --APPENDIX
-bikes.agg.ets <- bikes.hts %>% model(etsmod = ETS(Total)) %>% forecast()
-bikes.ets <- bikes.simplified %>% model(ETS(TotalBikes ~ error("A") + trend("N") + season("A")))
+bikes.agg.ets <- bikes.hts %>% model(AggregateETS = ETS(Total))
+bikes.ets <- bikes.simplified %>% model(SimpleETS = ETS(TotalBikes ~ error("A") + season("A")))
+
+fcast.agg.ets <- bikes.agg.ets %>% forecast(h = "5 days")
+fcast.ets <- bikes.ets %>% forecast(h = "5 days")
+
+accuracymatrix <- rbind(accuracymatrix, fcast.ets %>% accuracy(test.ts) %>% select(1:6), 
+                        fcast.agg.ets %>% accuracy(test.hts) %>% select(1:6))
 
 #show the forecast
-bikes.ets %>% filter(name == 'Nyon, Gare Nord') %>% forecast(h = 1000) %>% autoplot(bikes.simplified)
-
-#show the fitted values
-augment(bikes.ets) %>% filter(name == 'Nyon, Gare Nord') %>% 
-  ggplot(aes(x = date)) +
-  geom_line(aes(y = TotalBikes, colour = "Data"), size = 1) +
-  geom_line(aes(y = .fitted, colour = "Fitted"), size = 1) + xlab("Year") + ylab(NULL) +
-  guides(colour = guide_legend(title=NULL))                                                                  
-
-bikes.agg.ets
-
-#show the forecast for agg model
-bikes.agg.ets %>% forecast(h = "24 hours", method = 'bu') %>% autoplot(bikes.hts)
-#forecast appears usless
-
-#let's check last weeks data
+bikes.ets %>% filter(name == 'Nyon, Gare Nord') %>% forecast(h = "5 days") %>% autoplot(bikes.simplified)
+bikes.agg.ets %>% filter(name == 'Nyon, Gare Nord') %>% forecast(h = "5 days", method = 'bu') %>% autoplot(bikes.hts)
+                                                             
 
 #how about a simple ARIMA? --APPENDIX
 bikes.hts %>% features(Total, unitroot_kpss)
 bikes.hts %>% mutate(diff_Total = difference(Total)) %>% features(diff_Total, unitroot_kpss)
 bikes.hts %>% features(Total, unitroot_ndiffs)
 
-fit <- bikes.hts %>% model(ARIMA(Total))
-report(fit)                                   
-fit %>% forecast(h = 240) %>% autoplot(bikes.hts)
-                               
-bikes.hts %>% gg_tsdisplay(plot_type = "partial")
-#also a fairly useless forecast
+bikes.arima <- bikes.simplified %>% model(ARIMA(TotalBikes))
+fcast.arima <- bikes.arima %>% forecast(h = "5 days")
+fcast.arima %>% accuracy(test.ts)
+fcast.arima %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified)
+accuracymatrix <- rbind(accuracymatrix, fcast.arima %>% accuracy(test.ts) %>% select(1:6))
 
 
+#getting the accuracies of all the models
+accuracymatrix %>% group_by(.model) %>% summarize(ME = mean(ME), RMSE = mean(RMSE), MAE = mean(MAE))
 
-#let's try DYNAMIC REGRESSION now --APPENDIX
-dr.agg <- bikes.hts %>% model(ARIMA(Total ~ weekday + totalSnow_cm + uvIndex + HeatIndexC + WindChillC +
-                                      WindGustKmph + cloudcover + humidity + precipMM + maxtempC + visibility + windspeedKmph +
-                                      timeofday + holiday))
+#plotting the models to see the confidence intervals. ETS and ARIMA perform really poorly
+p1 <- fcast.lm %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('TSLM Model')
+p2 <- fcast.stl %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('STL Model')
+p3 <- fcast.ets %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('ETS Model')
+p4 <- fcast.arima %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('ARIMA')
 
-augment(dr.agg) %>% 
-  ggplot(aes(x = date)) +
-  geom_line(aes(y = Total, colour = "Data"), size = 1) +
-  geom_line(aes(y = .fitted, colour = "Fitted"), size = 1) + xlab("Year") + ylab(NULL) +
-  guides(colour = guide_legend(title=NULL))  
+(p1 | p2) / (p3 | p4)
 
-#horrible overfit
+#testing accuracy based on just 24 hour forecast
+oneday <- newdata.ts %>% filter(as.Date(date) < as.Date("2022-05-12"))
+oneday
 
-#So, either the aggregated TSLM model or the regular TSLM model can be used for forecasting. The next step is to just forecast
+#creating the forecasts
+m1 <- bikes.lm %>% forecast(new_data = oneday, h = "1 day") 
+m2 <- fc %>% forecast(h = "1 day") 
+m3 <- bikes.ets %>% forecast(h = '1 day')
+m4 <- bikes.arima %>% forecast(h = '1 day')
 
-#separate models for bikes and e-bikes: --APPENDIX
-bikes.ts.lm <- bikes.ts %>% model(TSLM(Bike ~ weekday + totalSnow_cm + uvIndex + HeatIndexC + WindChillC +
-                                              WindGustKmph + cloudcover + humidity + precipMM + maxtempC + visibility + windspeedKmph +
-                                              timeofday + holiday))
+#plotting
+c1 <- m1 %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('TSLM Model')
+c2 <- m2 %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('STL Model')
+c3 <- m3 %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('ETS Model')
+c4 <- m4 %>% filter(name == 'Nyon, Gare Nord') %>% autoplot(bikes.simplified) + ggtitle('ARIMA')
 
-#fitting the values
-augment(bikes.ts.lm) %>% filter(name == 'Nyon, Gare Sud') %>% 
-  ggplot(aes(x = date)) +
-  geom_line(aes(y = Bike, colour = "Data"), size = 1) +
-  geom_line(aes(y = .fitted, colour = "Fitted"), size = 1) + xlab("Year") + ylab(NULL) +
-  guides(colour = guide_legend(title=NULL))
+(c1 | c2) / (c3 | c4)
 
-#checking the report
-bikes.ts.lm %>% filter(name == 'Nyon, Gare Sud') %>% report()
+#testing the accuracy
+a <- rbind(
+          m1 %>% accuracy(test.ts) %>% select(1:6),
+          m2 %>% accuracy(test.ts) %>% select(1:6),
+          m3 %>% accuracy(test.ts) %>% select(1:6),
+          m4 %>% accuracy(test.ts) %>% select(1:6))
 
-#creating a model for e-bikes
-ebikes.ts.lm <- bikes.ts %>% model(TSLM(`E-Bike` ~ weekday + totalSnow_cm + uvIndex + HeatIndexC + WindChillC +
-                                         WindGustKmph + cloudcover + humidity + precipMM + maxtempC + visibility + windspeedKmph +
-                                         timeofday + holiday))
-#fitting the values to existing values
-augment(ebikes.ts.lm) %>% filter(name == 'Nyon, Gare Sud') %>% 
-  ggplot(aes(x = date)) +
-  geom_line(aes(y = `E-Bike`, colour = "Data"), size = 1) +
-  geom_line(aes(y = .fitted, colour = "Fitted"), size = 1) + xlab("Year") + ylab(NULL) +
-  guides(colour = guide_legend(title=NULL))
-
-#checking the report
-ebikes.ts.lm %>% filter(name == 'Nyon, Gare Sud') %>% report()
-
-#R-Squared for each of the models --APPENDIX
-reportbikes <- report(bikes.ts.lm)
-reportebikes <- report(ebikes.ts.lm)
-reporttotal <- report(bikes.lm)
-reportagg <- report(bikes.lm.agg)
-
-#let's compare the Adjusted-Rsquared of all the main models under consideration
-mean(reportbikes$adj_r_squared)
-mean(reportebikes$adj_r_squared)
-mean(reporttotal$adj_r_squared)
-mean(reportagg$adj_r_squared)
+#seeing the accuracy. The DCMP model outperforms all easily
+a %>% group_by(.model) %>% summarize(ME = mean(ME), RMSE = mean(RMSE), MAE = mean(MAE))
 
 
-#All right, now that we've done all of that nonsense, let's forecast!
-newdata <- read.csv("newdata.csv")
-newdata <- newdata %>% select(-c('tempC', 'mintempC', 'sunHour', 'moon_illumination', 'moonrise', 'moonset', 'sunrise', 'sunset',
-                                             'DewPointC', 'pressure', 'FeelsLikeC', 'winddirDegree', 'location'))
+#we've chosen our model. Now, it remains to be seen whether we want to do an aggregated one, 
+#a bike/e-bike separate one or combined one. There are four models to choose from here.
+# 1 - Model divided into regions but not into bikes/e-bikes
+# 2 - Model divided into regions and into bikes/e-biks
+# 3 - Model HTS but not bike/-ebik
+# 4 - Model HTS but into bike/e-bike
 
-#adding the relevant columns
-newdata$date <- as.POSIXct(newdata$date_time, format = "%d/%m/%Y %H:%M", tz = "UTC")
-newdata$timeofday <- as.factor(nightday(newdata$date))
-newdata$weekday <- as.factor(ifelse(isWeekday(newdata$date), "Weekday", "Weekend"))
-newdata$holiday <- 0
+#we've done 1. So let's do 2,3,4 and decide. We'll be using the total.ts dataset
 
-#turning it into a tsibble and getting rid of date_time since we have date
-newdata.ts <- as_tsibble(newdata, index = date, key = name)
-newdata.ts <- newdata.ts %>% select(-date_time)
+#creating an aggregated dataset
+total.hts <- total.ts %>% aggregate_key(name, TotalBikes = sum(TotalBikes))
+test.hts <- test.ts %>% aggregate_key(name, TotalBikes = sum(TotalBikes))
 
-fcast.lm <- forecast(bikes.lm, new_data = newdata.ts)
-fcast.lm
+hts.components <- total.hts %>%
+                      model(
+                        STL(TotalBikes,
+                            robust = TRUE)
+                      ) %>%
+                      components()
 
-nyon <- fcast.lm %>% filter(name == 'Nyon, Changins')
-bikes.simplified %>% filter(name == 'Nyon, Changins') %>%
-  autoplot(TotalBikes) +
-  autolayer(nyon)
+hts_dcmp_spec <- decomposition_model(
+  STL(TotalBikes,
+      robust = TRUE),
+  ETS(season_adjust ~ season("N"))
+)
 
-#we have high multi-collinearity, therefore, we need to check if the variables we have are perfectly correlated and remove sum:
-#the problem is with timeofday, let's try two models and see if they're worth removing
+htsfcast <- total.hts %>% model(base = hts_dcmp_spec)
+accmatrix <- htsfcast %>% reconcile(bu = bottom_up(base)) %>% forecast(h = "1 day") %>% accuracy(test.hts)
 
-bikes.simplified %>% ggplot() + geom_jitter(aes(x = maxtempC, y = totalSnow_cm))
-#snow is mostly zero, worth removing
+######################################
 
-bikes.simplified %>% ggplot() + geom_jitter(aes(x = maxtempC, y = uvIndex))
-#not perfectly correlated but close
+#FINAL COMPARISON. HTS OR NON-HTS
 
-bikes.simplified %>% ggplot() + geom_jitter(aes(x = maxtempC, y = HeatIndexC))
-#very linear trend. worth removing
+final_acc <- rbind(accuracymatrix %>% filter(.model == "my_dcmp_spec"), accmatrix %>% select(1:6))
+final_acc %>% group_by(.model) %>% summarize(ME = mean(ME), RMSE = mean(RMSE), MAE = mean(MAE))
+
+ME <- cbind(accuracymatrix %>% filter(.model == "my_dcmp_spec") %>% select(1,4),
+  accmatrix %>% filter(!is_aggregated(name) & .model == "bu") %>% select(1,2,4))
+
+RMSE <- cbind(accuracymatrix %>% filter(.model == "my_dcmp_spec") %>% select(1,5),
+              accmatrix %>% filter(!is_aggregated(name) & .model == "bu") %>% select(1,2,5))
+
+RMSE
+
+#FINAL FORECAST
+
+#the aggregated hts model outperforms the regular model by some distance using both RMSE and MAE
+
+#using the entire dataset to recreate model:
+  
+all.ts = as_tsibble(total, index = date, key = name, regular = TRUE)
+all.ts <- all.ts %>% select(!Capacity)
+all.ts <- all.ts %>% fill_gaps(.full = TRUE)
+all.ts <- na.locf(all.ts)
+all.ts %>% scan_gaps()
+all.ts <- all.ts %>% mutate(TotalBikes = Bike + `E-Bike`)
+
+all.hts <- all.ts %>% aggregate_key(name, TotalBikes = sum(TotalBikes))
+all.hts
+
+hts.components <- all.hts %>%
+  model(
+    STL(TotalBikes,
+        robust = TRUE)
+  ) %>%
+  components()
+
+hts_dcmp_spec <- decomposition_model(
+  STL(TotalBikes,
+      robust = TRUE),
+  ETS(season_adjust ~ season("N"))
+)
+
+htsfcast <- all.hts %>% model(base = hts_dcmp_spec)
+finalforecast <- htsfcast %>% reconcile(bu = bottom_up(base)) %>% forecast(h = "9 days")
+
+finalforecast %>% filter(is_aggregated(name) & .model == 'bu') %>% autoplot(all.hts, level = 90)
+export <- finalforecast %>% filter(is_aggregated(name) & .model == 'bu') %>% hilo(90)
+export
+
+write.csv(export, "forecast.csv")
 
